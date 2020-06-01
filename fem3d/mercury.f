28e7d43dd3ecb7154bd18817b9e9ecc6bb3f3b02
ginevrar <grosati@fluxus.ogs.trieste.it>
2020-06-01 11:23:22 +0200

    tCE prova

--- fem3d/mercury.f
+++ fem3d/mercury.f
@@ -1007,8 +1007,8 @@ c*************************************************************
 
         do ie=1,nel
           ia = iarv(ie)
-          if( ia == 0 ) tce = 0.01     !FIXME
-          if( ia== 1 )  tce = 0.01
+          if( ia == 0 ) tce = 0.03     !FIXME
+          if( ia== 1 )  tce = 0.03
           if( ia== 2 )  tce = 0.05
           if( ia== 3 )  tce = 0.2
           if( ia== 4 )  tce = 0.2
