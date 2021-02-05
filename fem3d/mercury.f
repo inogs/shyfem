5a1db6134f5905126577b0c105a710190ba5c662
ginevrar <ginevra.rosati@gmail.com>
2021-02-05 19:05:05 +0100

    tcebas

--- fem3d/mercury.f
+++ fem3d/mercury.f
@@ -1190,8 +1190,8 @@ c*************************************************************
 
         do ie=1,nel
           ia = iarv(ie)
-          if( ia == 0)  tce =0.5       !FIXME
-          if( ia== 1 )  tce =0.6
+          if( ia == 0)  tce =0.7       !FIXME
+          if( ia== 1 )  tce =0.7
 
           if( ia== 2 )  tce =.8
           if( ia== 6 )  tce =.8
